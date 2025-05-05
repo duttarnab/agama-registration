3430ef36628213b707e0856bfaea5e3c05c07f65
Arnab Dutta <arnab.bdutta@gmail.com>
2025-05-05 15:11:43 +0530

    refactor: update UserRegistration.java

--- lib/org/gluu/agama/user/UserRegistration.java
+++ lib/org/gluu/agama/user/UserRegistration.java
@@ -1,7 +1,6 @@
 package org.gluu.agama.user;
 
 import java.util.Map;
-
 import org.gluu.agama.registration.JansUserRegistration;
 
 
@@ -12,7 +11,11 @@ public abstract class UserRegistration {
     public abstract boolean usernamePolicyMatch(String userName);
 
     public abstract boolean passwordPolicyMatch(String userPassword);
-    
+
+    public abstract String updateUser(Map<String, String> profile) throws Exception;
+
+    public abstract Map<String, String> getUserEntityByInum(String inum);
+
     public static UserRegistration getInstance(){
         return  JansUserRegistration.getInstance();
     }    
